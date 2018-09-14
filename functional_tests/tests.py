ed3c63687c6f1e1d6b087f72ed05742971b81e7c
noodles <924461845@qq.com>
2018-09-14 10:08:20 +0800

    new_list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -48,8 +48,9 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # 伊丽丝新建一个待办事项清单

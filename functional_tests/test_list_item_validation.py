6d0a07a1d604e4aea3d474c5d76f8c971e3b80a7
noodles <924461845@qq.com>
2018-10-09 00:47:40 +0800

    use form in all views, back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -10,21 +10,24 @@ class ItemValidationTest(FunctionalTest):
         self.browser.get(self.live_server_url)
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            "You can't have an empty list item"))
+        # 浏览器截获了请求
+        # 清单页面不会加载
+        self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:invalid'))
 
+        # 输入文字，错误消失
         self.get_item_input_box().send_keys('Buy milk')
+        self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:valid'))
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
+
         # 再次输入空列表
         self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:invalid'))
 
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            "You can't have an empty list item"))
-
+        # 输入文字后纠正错误
         self.get_item_input_box().send_keys('Make tea')
+        self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:valid'))
+
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
